1db65f068fc2308a0d627017c7c39ce240571965
Pongsak <zajengza@gmail.com>
2025-06-14 15:02:33 +0700

    Step 6: Image widget

--- lib/screen/home.dart
+++ lib/screen/home.dart
@@ -3,108 +3,19 @@ import 'package:flutter/material.dart';
 class Home extends StatelessWidget {
   const Home({super.key});
 
-  // Step 5: container, color, main axis, cross axis
   @override 
   Widget build(BuildContext context) {
-//     return Column(
-//       // mainAxisAlignment: MainAxisAlignment.start,
-//       // mainAxisAlignment: MainAxisAlignment.center,
-//       // mainAxisAlignment: MainAxisAlignment.end,
-//       // mainAxisAlignment: MainAxisAlignment.spaceBetween,
-//       // mainAxisAlignment: MainAxisAlignment. spaceAround,
-//       // mainAxisAlignment: MainAxisAlignment.EvenlySpaced,
-//       mainAxisAlignment: MainAxisAlignment.spaceBetween,
-//       crossAxisAlignment: CrossAxisAlignment.stretch,
-      
-//       children: [
-//         Container(
-//           color: Colors.orangeAccent,
-//           padding: EdgeInsets.symmetric(horizontal: 5, vertical: 5),
-//           child: Text(
-//             "Hello Flutter",
-//             style: TextStyle(
-//               fontSize: 20,
-//               letterSpacing: 3,
-//               color: Colors.white,
-//             ),
-//           ),
-//         ),
-//         Container(
-//           color: Colors.orangeAccent,
-//           padding: EdgeInsets.symmetric(horizontal: 5, vertical: 5),
-//           child: Text(
-//             "Hello Flutter",
-//             style: TextStyle(
-//               fontSize: 20,
-//               letterSpacing: 3,
-//               color: Colors.white,
-//             ),
-//           ),
-//         ),
-//         Container(
-//           color: Colors.orangeAccent,
-//           padding: EdgeInsets.symmetric(horizontal: 5, vertical: 5),
-//           child: Text(
-//             "Hello Flutter",
-//             style: TextStyle(
-//               fontSize: 20,
-//               letterSpacing: 3,
-//               color: Colors.white,
-//             ),
-//           ),
-//         ),
-//       ],
-//     );
-//   }
-// }
-
-
- return Row(
-      // mainAxisAlignment: MainAxisAlignment.start,
-      // mainAxisAlignment: MainAxisAlignment.center,
-      // mainAxisAlignment: MainAxisAlignment.end,
-      // mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      // mainAxisAlignment: MainAxisAlignment. spaceAround,
-      // mainAxisAlignment: MainAxisAlignment.EvenlySpaced,
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+    // Step 6: Image widget
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
       crossAxisAlignment: CrossAxisAlignment.stretch,
-      
       children: [
-        Container(
-          color: Colors.orangeAccent,
-          padding: EdgeInsets.symmetric(horizontal: 5, vertical: 5),
-          child: Text(
-            "Hello",
-            style: TextStyle(
-              fontSize: 20,
-              letterSpacing: 3,
-              color: Colors.white,
-            ),
-          ),
-        ),
-        Container(
-          color: Colors.orangeAccent,
-          padding: EdgeInsets.symmetric(horizontal: 5, vertical: 5),
-          child: Text(
-            "Hello Flutter",
-            style: TextStyle(
-              fontSize: 20,
-              letterSpacing: 3,
-              color: Colors.white,
-            ),
-          ),
-        ),
-        Container(
-          color: Colors.orangeAccent,
-          padding: EdgeInsets.symmetric(horizontal: 5, vertical: 5),
-          child: Text(
-            "Hello Flutter",
-            style: TextStyle(
-              fontSize: 20,
-              letterSpacing: 3,
-              color: Colors.white,
-            ),
-          ),
+        Image.asset("images/Rx7.jpg", width: 150, height: 150),
+        const SizedBox(height: 10),
+        Image.network(
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/3/3c/Mazda_RX-7_2.jpg/800px-Mazda_RX-7_2.jpg",
+          width: 150,
+          height: 150,
         ),
       ],
     );
